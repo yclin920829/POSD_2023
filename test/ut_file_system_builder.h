39a6eb9b9bc0bab3e20b5157dae15e3e8500d847
林于喬 <t110820059@ntut.org.tw>
2023-11-12 16:03:06 +0800

    check scanner output 2.

--- test/ut_file_system_builder.h
+++ test/ut_file_system_builder.h
@@ -51,37 +51,37 @@ TEST(FileSystemBuilder, scanner){
         std::cout << scanner->currentNodeName() << std::endl;
     }
 
-    // scanner->setPath("data/home");
+    scanner->setPath("data/home");
 
-    // ASSERT_EQ(scanner->currentNodeName(), "my_profile");
-    // ASSERT_TRUE(scanner->isFile());
-    // ASSERT_FALSE(scanner->isFolder());
-    // ASSERT_FALSE(scanner->isDone());
+    ASSERT_EQ(scanner->currentNodeName(), "my_profile");
+    ASSERT_TRUE(scanner->isFile());
+    ASSERT_FALSE(scanner->isFolder());
+    ASSERT_FALSE(scanner->isDone());
 
-    // scanner->nextNode();
+    scanner->nextNode();
 
-    // ASSERT_EQ(scanner->currentNodeName(), "Documents");
-    // ASSERT_FALSE(scanner->isFile());
-    // ASSERT_TRUE(scanner->isFolder());
-    // ASSERT_FALSE(scanner->isDone());
+    ASSERT_EQ(scanner->currentNodeName(), "hello.txt");
+    ASSERT_FALSE(scanner->isFile());
+    ASSERT_TRUE(scanner->isFolder());
+    ASSERT_FALSE(scanner->isDone());
 
-    // scanner->nextNode();
+    scanner->nextNode();
 
-    // ASSERT_EQ(scanner->currentNodeName(), "Downloads");
-    // ASSERT_FALSE(scanner->isFile());
-    // ASSERT_TRUE(scanner->isFolder());
-    // ASSERT_FALSE(scanner->isDone());
+    ASSERT_EQ(scanner->currentNodeName(), "Documents");
+    ASSERT_FALSE(scanner->isFile());
+    ASSERT_TRUE(scanner->isFolder());
+    ASSERT_FALSE(scanner->isDone());
 
-    // scanner->nextNode();
+    scanner->nextNode();
 
-    // ASSERT_EQ(scanner->currentNodeName(), "hello.txt");
-    // ASSERT_TRUE(scanner->isFile());
-    // ASSERT_FALSE(scanner->isFolder());
-    // ASSERT_FALSE(scanner->isDone());
+    ASSERT_EQ(scanner->currentNodeName(), "Downloads");
+    ASSERT_TRUE(scanner->isFile());
+    ASSERT_FALSE(scanner->isFolder());
+    ASSERT_FALSE(scanner->isDone());
 
-    // scanner->nextNode();
+    scanner->nextNode();
 
-    // ASSERT_TRUE(scanner->isDone());
+    ASSERT_TRUE(scanner->isDone());
 }
 
 TEST(FileSystemBuilder, builder){
