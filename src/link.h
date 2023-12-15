b7f8e1d49f882e8df1450a65c825934888cc8539
林于喬 <t110820059@ntut.org.tw>
2023-12-15 19:58:16 +0800

    wish to make the ta's compiler compile successfully 2.

--- src/link.h
+++ src/link.h
@@ -1,18 +1,26 @@
 #pragma once
 
+#include "file.h"
 #include "node.h"
 
 class Link: public Node {
 public:
 
     // TODO: implement it
-    int numberOfFiles() const override {}
+    int numberOfFiles() const override {
+        return 0;
+    }
 
     // TODO: implement it
-    Node * find(string path) override {}
+    Node * find(string path) override {
+        return new File(path);
+    }
 
     // TODO: implement it
-    std::list<string> findByName(string name) override {}
+    std::list<string> findByName(string name) override {
+        std::list<string> pathList;
+        return pathList;
+    }
 
     // TODO: implement it
     void add(Node * node) override {}
@@ -21,10 +29,14 @@ public:
     void remove(string patth) override {}
 
     // TODO: implement it
-    Node * getChildByName(const char * name) const override {}
+    Node * getChildByName(const char * name) const override {
+        return new File(name);
+    }
 
     // TODO: implement it
-    Node * getTarget() {}
+    Node * getTarget() {
+        return new File("not done yet");
+    }
 
     // TODO: implement it
     void accept(Visitor * guest) override {}
