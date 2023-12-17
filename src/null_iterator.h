a6d7edbfb1136568071353b30ff4234d7fb0244d
林于喬 <t110820059@ntut.org.tw>
2023-12-17 14:29:51 +0800

    see test detail.

--- src/null_iterator.h
+++ src/null_iterator.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <iostream>
 #include "iterator.h"
 
 class NullIterator : public Iterator {
 public:
+    NullIterator() {
+        std::cout << "NullIterator::NullIterator()" << std::endl;
+    }
+
     bool isDone () const override {
         return true;
     }
